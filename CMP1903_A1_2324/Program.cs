694a8029807a8a62523a2e7a49dd9b9d01dbbf2c
JustinasUrb <justas1101@gmail.com>
2024-03-01 10:51:38 +0000

    Final Draft For Review

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -8,10 +8,10 @@ namespace CMP1903_A1_2324
 {
     internal class Program
     {
-        static void Main(string[] args) //Main is used first, as all the functions of this program start from here.
+        static void Main(string[] args) //Main is used first, as all the functions of this program start from here
         {
-            Game DieRoller = new Game(); //creates a game object called 'DieRoller'.
-            DieRoller.DieGame(); //calls the DieGame() method, which is the starting point of the program.
+            Game DieRoller = new Game(); //creates a game object called 'DieRoller'
+            DieRoller.DieGame(); //calls the DieGame() method, which is the starting point of the program
         }
     }
 }
