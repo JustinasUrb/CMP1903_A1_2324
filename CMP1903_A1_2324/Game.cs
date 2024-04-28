9a5f864322042771c54eb4607e947603bdab7c4e
JustinasUrb <justas1101@gmail.com>
2024-04-28 14:26:41 +0100

    Assessment 2 - Added statistics functionality for keeping record of scores through program runtime.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -60,6 +60,8 @@ namespace CMP1903_A1_2324
     internal class SevensOut
     {
         private static Testing test = new Testing();
+        public static List<int> scores = new List<int>(); // List to store scores of each game
+
         public void SevensOutGame()
         {
             bool StoppingGame = false;
@@ -67,13 +69,13 @@ namespace CMP1903_A1_2324
             int TotalRollValue = 0;
             while (true)
             {
-                Die dieOne = new Die(); //creates a die object, which will be used to roll a number between 1 and 6
-                dieOne.Roll(); //uses the die object to start a unique roll method for it
-                int DieOneValue = dieOne.DieValue; //returned integer is set as a value for the die object
+                Die dieOne = new Die();
+                dieOne.Roll();
+                int DieOneValue = dieOne.DieValue;
 
-                Die dieTwo = new Die(); //creates a new die object
-                dieTwo.Roll(); //uses the die object to start a unique roll method for it
-                int DieTwoValue = dieTwo.DieValue; //returned integer is set as a value for this unique die object
+                Die dieTwo = new Die();
+                dieTwo.Roll();
+                int DieTwoValue = dieTwo.DieValue;
 
                 if (DieOneValue == DieTwoValue)
                 {
@@ -84,23 +86,28 @@ namespace CMP1903_A1_2324
                     TotalRollValue = DieOneValue + DieTwoValue;
                 }
 
-                TotalValue = TotalValue += TotalRollValue;
+                TotalValue += TotalRollValue;
                 Console.WriteLine($"\nDie rolls are: {DieOneValue} and {DieTwoValue}\nTotal (current) throw value: {TotalRollValue}\nTotal: {TotalValue}");
                 Console.ReadKey();
+
                 if (TotalRollValue == 7)
                 {
                     StoppingGame = true;
                     test.SevensOutGame(TotalRollValue, StoppingGame);
                     Console.WriteLine($"\n\nA '7' was rolled!\nFinal Score: {TotalValue}\n\n");
+                    scores.Add(TotalValue); // Add final score to the list
                     break;
                 }
             }
         }
     }
 
+
     internal class ThreeOrMore
     {
         private static Testing test = new Testing();
+        public static List<int> playerOneScores = new List<int>();
+        public static List<int> playerTwoOrComputerScores = new List<int>();
         public void ThreeOrMoreMenu()
         {
             int players = 0; 
@@ -109,9 +116,9 @@ namespace CMP1903_A1_2324
             {
                 try
                 {
-                    Console.WriteLine("\n\nPlease enter a number based on the action you want to occur: \n\n1 = Play Singleplayer (against a computer)\n2 = Play Multiplayer (against a person)\n\nMake your choice below:");
+                    Console.WriteLine("\n\nPlease enter a number based on the action you want to occur: \n\n1 = Play Singleplayer (against a computer)\n2 = Play Multiplayer (against a person)\n3 = Exit back to Menu\n\nMake your choice below:");
                     int playerChoice = int.Parse(Console.ReadLine());
-                    if (playerChoice < 1 || playerChoice > 2)
+                    if (playerChoice < 1 || playerChoice > 3)
                     {
                         Console.WriteLine("Please remember to put an integer that is either a: 1 or 2");
                     }
@@ -124,6 +131,10 @@ namespace CMP1903_A1_2324
                     {
                         players = 2;
                         ThreeOrMoreGame(players);
+                    }
+                    else if(playerChoice == 3)
+                    {
+                        break;
                     }
                 }
                 catch (Exception e)
@@ -251,6 +262,9 @@ namespace CMP1903_A1_2324
 
                 if (playerOneScore >= 20 || computerOrPlayerTwoScore >= 20)
                 {
+                    playerOneScores.Add(playerOneScore);
+                    playerTwoOrComputerScores.Add(computerOrPlayerTwoScore);
+
                     Console.WriteLine($"{(playerOneScore >= 20 ? "Player 1" : (players == 1 ? "Computer" : "Player 2"))} wins!");
                     break;
                 }
@@ -274,32 +288,69 @@ namespace CMP1903_A1_2324
             {
                 try
                 {
-                    Console.WriteLine("Please enter a number based on the action you want to occur: \n\n1 = See 'Sevens Out' Statistics\n2 = See 'Three or More' Statistics\n3 = Exit back to Menu\n\nMake your choice below:");
+                    Console.WriteLine("\n\nPlease enter a number based on the action you want to occur: \n\n1 = See 'Sevens Out' Statistics\n2 = See 'Three or More' Statistics\n3 = Exit back to Menu\n\nMake your choice below:");
                     int statChoice = int.Parse(Console.ReadLine());
                     if (statChoice < 1 || statChoice > 3)
                     {
-                        Console.WriteLine("Please remember to put an integer that is either a: 1 or 2");
+                        Console.WriteLine("Please remember to put an integer that is either: 1, 2, or 3");
                     }
                     else if (statChoice == 1)
                     {
-                        //See 'Sevens Out' Statistics
-                        Console.WriteLine("sevens out stats");
+                        // Display 'Sevens Out' scores
+                        if (!SevensOut.scores.Any())
+                        {
+                            Console.WriteLine("No 'Sevens Out' games have been played yet.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nScores from 'Sevens Out' games:");
+                            foreach (int score in SevensOut.scores)
+                            {
+                                Console.WriteLine(score);
+                            }
+                        }
                     }
                     else if (statChoice == 2)
                     {
-                        //See 'Three or more' Statistics
-                        Console.WriteLine("three or more stats");
+                        // Display 'Three or More' scores
+                        Console.WriteLine("\n\nScores from 'Three or More' games:\n");
+                        if (!ThreeOrMore.playerOneScores.Any())
+                        {
+                            Console.WriteLine("No games have been played yet for Player 1.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 1 Scores:");
+                            foreach (int score in ThreeOrMore.playerOneScores)
+                            {
+                                Console.WriteLine(score);
+                            }
+                        }
+
+                        if (!ThreeOrMore.playerTwoOrComputerScores.Any())
+                        {
+                            Console.WriteLine("No games have been played yet for Player 2 or Computer.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 2 / Computer Scores:");
+                            foreach (int score in ThreeOrMore.playerTwoOrComputerScores)
+                            {
+                                Console.WriteLine(score);
+                            }
+                        }
                     }
                     else if (statChoice == 3)
                     {
-                        break;
+                        break; // Exit back to Menu
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"\n\nError: {e}\n\nPlease remember to put an integer that is either a: 1 or 2");
+                    Console.WriteLine($"\n\nError: {e}\n\nPlease remember to put an integer that is either a: 1, 2, or 3");
                 }
             }
         }
     }
+
 }
